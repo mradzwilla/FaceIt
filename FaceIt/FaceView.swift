c85993461688d17dde2de52a1701e963dc47887f
mradzwilla <mradzwilla@gmail.com>
2017-03-20 01:26:26 -0400

    Basic features added.

---
+++ FaceIt/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by Michael Radzwilla on 3/20/17.
+//  Copyright © 2017 swifttutorial. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
